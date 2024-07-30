709b1cf9e18d5ec3b05be949310dcb8b8ceee952
bhavyaainapure <91969480+bhavyaainapure@users.noreply.github.com>
2024-07-30 18:16:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.write('hello')
+st.write('This application is not part of the Snowflake Service and is governed by the terms in LICENSE, unless expressly agreed to in writing. You use this application at your own risk, and Snowflake has no obligation to support your use of this application.')
